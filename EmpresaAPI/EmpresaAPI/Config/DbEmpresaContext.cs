9608b8aa885909b3a352a3acebda24ec8a747abe
Ivan E <ivan.eirea@softwareone.com>
2023-01-10 10:44:57 +0100

    First commit

---
+++ EmpresaAPI/EmpresaAPI/Config/DbEmpresaContext.cs
@@ -0,0 +1,18 @@
+﻿using EmpresaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpresaAPI.Config
+{
+    public class DbEmpresaContext : DbContext
+    {
+        public DbEmpresaContext(DbContextOptions<DbEmpresaContext> options) : base(options)
+        {
+
+        }
+
+        // Empleados es el nombre que tendrá la base de datos, y al DbSet le pasamos por el operador diamante el nombre del modelo que representa a dicha tabla en nuestro back
+        public DbSet<Empleado> Empleados { get; set; }
+
+
+    }
+}
---
+++ EmpresaAPI/EmpresaAPI/Controllers/EmpleadoController.cs
@@ -0,0 +1,33 @@
+﻿using EmpresaAPI.Config;
+using EmpresaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpresaAPI.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly DbEmpresaContext _context;
+        public EmpleadoController(DbEmpresaContext context)
+        {
+            _context = context;
+        }
+
+        /*SAVE*/
+        [HttpPost("save")]
+        public async Task<ActionResult> Post(Empleado empleado)
+        {
+            _context.Add(empleado); //Añadir al contexto
+            await _context.SaveChangesAsync(); //Guardar async, tiene que await
+            return Ok(); //Retorna status code 200 si todo ok
+        }
+
+        /*LIST*/
+        [HttpGet("list")]
+        public async Task<ActionResult<List<Empleado>>> Get(Empleado empleado)
+        {
+            return await _context.Empleados.ToListAsync();
+        }
+
+    }
+}
